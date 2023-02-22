e5ec979ebd969b106033c19a07bf4987b9c3d8cc
Moroz Tetiana <tania@Morozs-MacBook-Air.local>
2023-02-22 16:15:09 +0200

    expandable tree with hidden and showed elements

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-1. Replace `<your_account>` with your Github username in the link
+1. Replace `<Tania-del>` with your Github username in the link
     - [DEMO LINK](https://<your_account>.github.io/js_sliding-tree-DOM/)
 2. Follow [this instructions](https://mate-academy.github.io/layout_task-guideline/)
     - Run `npm run test` command to test your code;
@@ -9,7 +9,7 @@
 
 Create an expandable tree.
 
-Requirements: 
+Requirements:
 - Clicking on the headline should hide or show elements.
 - Clicking outside the title text (in an empty place) should do nothing.
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const { children } = tree;
+
+for (const el of children) {
+  el.classList.add('header');
+}
+
+const headers = document.querySelectorAll('.header');
+
+headers.forEach(header => {
+  const span = document.createElement('span');
+
+  span.textContent = header.childNodes[0].textContent;
+  header.childNodes[0].replaceWith(span);
+
+  const list = header.querySelector('ul');
+
+  span.addEventListener('click', () => {
+    if (list.style.display === '') {
+      list.style.display = 'none';
+    } else {
+      list.style.display = '';
+    }
+  });
+});
